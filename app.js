fc1e1a1d3e88e80f1759551acf6d5771f4588537
MN-dev <huntereyes93@gmail.com>
2023-04-24 21:16:08 +0200

    first commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = process.env || 8000;
 const helmet = require("helmet");
 
 app.set("view engine", "ejs");
---
+++ public/ScrollTop.js
@@ -0,0 +1,7 @@
+const scrollToTop = document.querySelector(".toTopBtn");
+scrollToTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+});
